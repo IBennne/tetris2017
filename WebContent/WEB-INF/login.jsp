7f2f7b773322b881c5d09cf3623e07f202da89ec
camille <tal.adoum@gmail.com>
2017-05-23 14:47:01 +0200

    acceuil 2

--- WebContent/WEB-INF/login.jsp
+++ WebContent/WEB-INF/login.jsp
@@ -1,12 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>login</title>
 </head>
 <body>
-
+	<br>
+	<h2>nom d'utilisateur</h2>
+	<form method="POST" action=>
+		<input type="text" name="username">
+		<input type="submit" value="se connecter">
+		<h2>Mot de passe</h2>
+		<input type="text" name="mot de passe"> <input type="submit"
+			name="ok">
+	</form>
 </body>
 </html>
--- src/tp/servlet/HomeServlet.java
+++ src/tp/servlet/HomeServlet.java
@@ -13,28 +13,44 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HomeServlet")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public HomeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf8");
+
+		// on regarde si l'utilisateur est connecté
+		if (request.getSession().getAttribute("username") == null) {
+			// accès non autorisé !
+			this.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+		} else {
+			// J'affiche la page de connexion
+			this.getServletContext().getRequestDispatcher("/WEB-INF/HomeConnexion.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String myUsername = request.getParameter("username");
+
+		request.getSession().setAttribute("username", myUsername);
 		doGet(request, response);
 	}
 
