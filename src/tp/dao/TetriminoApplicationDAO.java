ee8e0ca408465bdcb9982377f736a6bd7cffb30b
ajc <malbert.benoit@gmail.com>
2017-05-24 09:27:43 +0200

    TetriminoDAO

---
+++ src/tp/dao/TetriminoApplicationDAO.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tp.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tp.model.Tetrimino;
+
+/**
+ * @author ajc
+ *
+ */
+
+public class TetriminoApplicationDAO {
+	private static Map<String, Tetrimino> tetriminos = new HashMap<String, Tetrimino>();
+
+
+	public static List<Tetrimino> findAll() {
+		return new ArrayList<>(tetriminos.values());
+	}
+}
+	
+
